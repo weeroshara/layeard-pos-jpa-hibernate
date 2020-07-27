dc70469413dfdd93ff28859096bd84ecfa78c578
poornimavithanage <vithanagepurnima@gmail.com>
2020-07-27 18:36:38 +0530

    Finish ManageItemFormController

--- src/controller/ManageItemFormController.java
+++ src/controller/ManageItemFormController.java
@@ -5,10 +5,12 @@
  */
 package controller;
 
+import business.BusinessLogic;
 import com.jfoenix.controls.JFXTextField;
 import db.DBConnection;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -101,21 +103,10 @@ public class ManageItemFormController implements Initializable {
     }
 
     private void loadAllItems() {
-        try {
-            Statement stm = DBConnection.getInstance().getConnection().createStatement();
-            ResultSet rst = stm.executeQuery("SELECT * FROM Item");
-            ObservableList<ItemTM> items = tblItems.getItems();
-            items.clear();
-            while (rst.next()) {
-                String code = rst.getString(1);
-                String description = rst.getString(2);
-                double unitPrice = rst.getDouble(3);
-                int qtyOnHand = rst.getInt(4);
-                items.add(new ItemTM(code, description, qtyOnHand, unitPrice));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        ObservableList<ItemTM> items = tblItems.getItems();
+        items.clear();
+        items = FXCollections.observableArrayList(BusinessLogic.getAllItems());
+        tblItems.setItems(items);
     }
 
     @FXML
@@ -130,6 +121,7 @@ public class ManageItemFormController implements Initializable {
 
     @FXML
     private void btnSave_OnAction(ActionEvent event) {
+
 
         if (txtDescription.getText().trim().isEmpty() ||
                 txtQtyOnHand.getText().trim().isEmpty() ||
@@ -148,37 +140,18 @@ public class ManageItemFormController implements Initializable {
 
         if (btnSave.getText().equals("Save")) {
 
-            try {
-                PreparedStatement pstm = DBConnection.getInstance().getConnection().prepareStatement("INSERT INTO Item VALUES (?,?,?,?)");
-                pstm.setObject(1, txtCode.getText());
-                pstm.setObject(2, txtDescription.getText());
-                pstm.setObject(3, qtyOnHand);
-                pstm.setObject(4, unitPrice);
-                if (pstm.executeUpdate() == 0) {
-                    new Alert(Alert.AlertType.ERROR, "Failed to save the item", ButtonType.OK).show();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
+            boolean result = BusinessLogic.saveItem(txtCode.getText(), txtDescription.getText(), qtyOnHand, unitPrice);
+            if (!result){
+                new Alert(Alert.AlertType.ERROR, "Failed to save the item", ButtonType.OK).show();
             }
-
             btnAddNew_OnAction(event);
         } else {
             ItemTM selectedItem = tblItems.getSelectionModel().getSelectedItem();
 
-            try {
-                PreparedStatement pstm = DBConnection.getInstance().getConnection().prepareStatement("UPDATE Item SET description=?, unitPrice=?, qtyOnHand=? WHERE ItemCode=?");
-                pstm.setObject(1, txtDescription.getText());
-                pstm.setObject(2, unitPrice);
-                pstm.setObject(3, qtyOnHand);
-                pstm.setObject(4, selectedItem.getCode());
-                if (pstm.executeUpdate() == 0) {
-                    new Alert(Alert.AlertType.ERROR, "Failed to update the Item").show();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
+            boolean result = BusinessLogic.updateItem(txtDescription.getText(),  qtyOnHand, unitPrice, selectedItem.getCode());
+            if (!result) {
+                new Alert(Alert.AlertType.ERROR, "Failed to update the Item").show();
             }
-
-
             tblItems.refresh();
             btnAddNew_OnAction(event);
         }
@@ -193,17 +166,12 @@ public class ManageItemFormController implements Initializable {
         Optional<ButtonType> buttonType = alert.showAndWait();
         if (buttonType.get() == ButtonType.YES) {
             ItemTM selectedItem = tblItems.getSelectionModel().getSelectedItem();
-            try {
-                PreparedStatement pstm = DBConnection.getInstance().getConnection().prepareStatement("DELETE FROM Item WHERE ItemCode=?");
-                pstm.setObject(1, selectedItem.getCode());
-                if (pstm.executeUpdate() == 0) {
-                    new Alert(Alert.AlertType.ERROR, "Failed to delete the item", ButtonType.OK).show();
-                } else {
-                    tblItems.getItems().remove(selectedItem);
-                    tblItems.getSelectionModel().clearSelection();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
+            boolean result = BusinessLogic.deleteItem(selectedItem.getCode());
+            if (!result){
+                new Alert(Alert.AlertType.ERROR, "Failed to delete the item", ButtonType.OK).show();
+            }else{
+                tblItems.getItems().remove(selectedItem);
+                tblItems.getSelectionModel().clearSelection();
             }
         }
     }
