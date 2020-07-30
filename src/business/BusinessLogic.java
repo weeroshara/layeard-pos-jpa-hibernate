ead450c0a1fe88019bef2042db1924025cd36ba4
poornimavithanage <vithanagepurnima@gmail.com>
2020-07-30 15:43:00 +0530

    Apply generics to BusinessLogic file

--- src/business/BusinessLogic.java
+++ src/business/BusinessLogic.java
@@ -90,10 +90,9 @@ public class BusinessLogic {
 
     public static List<CustomerTM> getAllCustomers(){
         CustomerDAO customerDAO = new CustomerDAOImpl();
-        List<Object> allCustomers =customerDAO.findAll();
+        List<Customer> allCustomers =customerDAO.findAll();
         List<CustomerTM> customers = new ArrayList<>();
-        for (Object c: allCustomers) {
-            Customer customer = (Customer) c;
+        for (Customer customer: allCustomers) {
             customers.add(new CustomerTM(customer.getCustomerId(),customer.getCustomerName(),customer.getCustomerAddress()));
         }
         return customers;
@@ -116,10 +115,9 @@ public class BusinessLogic {
 
     public static List<ItemTM> getAllItems(){
         ItemDAO itemDAO = new ItemDAOImpl();
-        List<Object> allItems = itemDAO.findAll();
+        List<Item> allItems = itemDAO.findAll();
         List<ItemTM> items = new ArrayList<>();
-        for (Object i: allItems) {
-            Item item = (Item) i;
+        for (Item item: allItems) {
             items.add(new ItemTM(item.getItemCode(),item.getDescription(),item.getQtyOnHand(),item.getUnitPrice().doubleValue()));
         }
         return items;
